42ce026ca3c0944a72a347eae116b81f75d0006c
Phil Mazerolle <pmazerolle+github@pm.me>
2025-10-21 16:12:04 -0400

    Section 2.9 assignment

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,11 @@
+let country = "Canada";
+
+let continent = "North America";
+
+let population = "35 Million";
+
+console.log("Country = " + country);
+
+console.log("Continent is " + continent);
+
+console.log("Population is " + population);
