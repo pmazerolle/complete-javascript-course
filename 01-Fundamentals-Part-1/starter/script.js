49c76a48ef003176e823db907b53af158120f022
Phil Mazerolle <pmazerolle+github@pm.me>
2025-10-21 16:08:15 -0400

    Section 2.9

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,9 @@
-let js = 'amazing';
-if (js === "amazing") alert('JavaScript is FUN!');
-
+let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Fart";
+console.log(firstName);
+
