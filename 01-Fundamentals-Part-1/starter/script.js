0fea465738bd2ba4a27352d7d1903da9f751d691
Phil Mazerolle <pmazerolle+github@pm.me>
2025-10-22 16:17:16 -0400

    Section 2.21

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -190,7 +190,7 @@ if (markHigherBMI) {
 console.log(BMIMessage);
 */
 
-
+/* 
 // Section 2.20
 // Type conversion and coercion
 
@@ -210,3 +210,30 @@ console.log('23' + '10' + 3); // Coerced into String
 
 let n = '1' + 1; // '11'
 n = n - 1; // 11 - 1
+*/
+
+
+// Section 2.21
+// Truthy and Falsy Values
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+
+const money = -234;
+
+if (money) {
+    console.log("Don't spend it all");
+} else {
+    console.log("You should get a job");
+}
+
+let height;
+if (height) {
+    console.log('YAY! height is defined');
+} else {
+    console.log("Height is UNDEFINED");
+}
