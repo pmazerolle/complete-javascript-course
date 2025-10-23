e69bba91057e207b330dae737e3b83791385035e
Phil Mazerolle <pmazerolle+github@pm.me>
2025-10-23 11:04:41 -0400

    Section 3.35

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,7 +1,7 @@
 
 // Section 3.33
 'use strict';
-/* 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -13,7 +13,7 @@ if (hasDriversLicense) console.log("I can drive");
 // const private = 54;
 */
 
-
+/*
 // Section 3.34 - Functions
 function logger() {
     console.log("My name is fart");
@@ -32,5 +32,25 @@ console.log(appleJuice);
 console.log(fruitProcessor(5, 0));
 
 const appleOrangeJuice = fruitProcessor(2, 4);
+*/
+
+// Section 3.35 - Function Declarations vs Expressions
+// Declaration - can be defined later than it is called
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1988);
+
+console.log(age1);
+
+// Expression - must be initialized before use
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1988);
+
+console.log(age1, age2);
 
 
