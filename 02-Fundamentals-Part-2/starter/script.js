e9eb64cd2827739c3191dd5c087c4b0edc61cbe4
Phil Mazerolle <pmazerolle+github@pm.me>
2025-10-23 10:28:45 -0400

    Section 3.34

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,7 +1,7 @@
 
 // Section 3.33
 'use strict';
-
+/* 
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -11,3 +11,26 @@ if (hasDriversLicense) console.log("I can drive");
 
 // const interface = 'Audio';
 // const private = 54;
+*/
+
+
+// Section 3.34 - Functions
+function logger() {
+    console.log("My name is fart");
+}
+
+logger(); // calling, running, invoking function
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+
+
