e363e1e0bc4c36d05d53644f8757504a1d276a00
Phil Mazerolle <pmazerolle+github@pm.me>
2025-10-23 10:20:41 -0400

    Section 3.33

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+
+// Section 3.33
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+
+if (hasDriversLicense) console.log("I can drive");
+
+// const interface = 'Audio';
+// const private = 54;
